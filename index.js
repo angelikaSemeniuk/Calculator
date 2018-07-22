b929f78df7d85b27a85e2fe85aac9a888efb91b3
Anzhelika <angelika0094@gmail.com>
2018-07-22 16:35:26 +0300

    Complete all math operations

--- index.js
+++ index.js
@@ -3,26 +3,65 @@ var arrayOfElements = []; // [1, "+", 1]
 var enteredNumber = ""; // 111
 
 function clickOnButton(number) {
-    //arrayOfElements.push(number);
-    console.log(arrayOfElements);
     enteredNumber = enteredNumber + number;
+    console.log("enteredNumber", enteredNumber);
+
 }
 
 function clickOnSign(sign) {
-    arrayOfElements.push(+enteredNumber);
-    arrayOfElements.push(sign);
-    console.log(arrayOfElements);
+    if(enteredNumber) {
+        arrayOfElements.push(+enteredNumber);
+    }
+    if (typeof arrayOfElements[arrayOfElements.length-1] == "number") {
+        arrayOfElements.push(sign);
+    } else if (arrayOfElements[arrayOfElements.length-1] !== sign) {
+        arrayOfElements[arrayOfElements.length-1] = sign;
+    }
+
     enteredNumber = "";
 }
 
 function calculate() {
     arrayOfElements.push(+enteredNumber);
-    console.log(arrayOfElements);
     var res = 0;
-    for(var i = 0; i< arrayOfElements.length; i++) {
+    var madeFirstOperation = false;
+    for(var i = 0; i < arrayOfElements.length; i++) {
         if(arrayOfElements[i] == "+") {
-            res = arrayOfElements[i-1] + arrayOfElements[i+1];
+            if (madeFirstOperation) {
+                res += arrayOfElements[i+1];
+            } else {
+                res += (arrayOfElements[i-1] + arrayOfElements[i+1]);
+                madeFirstOperation = true;
+            }
+        }
+        if(arrayOfElements[i] == "-") {
+            if (madeFirstOperation) {
+                res -= arrayOfElements[i+1];
+            } else {
+                res += (arrayOfElements[i-1] - arrayOfElements[i+1]);
+                madeFirstOperation = true;
+            }
+        }
+        if(arrayOfElements[i] == "*") {
+            if (madeFirstOperation) {
+                res *= arrayOfElements[i+1];
+            } else {
+                res += (arrayOfElements[i-1] * arrayOfElements[i+1]);
+                madeFirstOperation = true;
+            }
+        }
+        if(arrayOfElements[i] == "/") {
+            if (madeFirstOperation) {
+                res /= arrayOfElements[i+1];
+            } else {
+                res += (arrayOfElements[i-1] / arrayOfElements[i+1]);
+                madeFirstOperation = true;
+            }
         }
     }
-    console.log(res);
+    console.log("res", res);
+    console.log("arrayOfElements", arrayOfElements);
+    arrayOfElements = [res];
+    enteredNumber = "";
+
 }
